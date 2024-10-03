622c7e3fae25bf1e689b033f44e6f039501e06f2
NunoSilva <104792409+NunoRNx@users.noreply.github.com>
2024-10-03 15:16:32 +0100

    Revisões Calc + Classes Studies

---
+++ Classes/ClassObject/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    class Pessoa
+    {
+        public int age {  get; set; }
+        public string name {  get; set; }
+        public string address { get; set; }
+        public int phone { get; set; }
+
+        #region Construtor
+        public Pessoa(int age, string name, string address, int phone)
+        {
+            if (age < 18)
+            {
+                Console.WriteLine("Must be 18 or older to sign up!");
+            }
+            else
+            {
+                this.age = age;
+                this.name = name;
+                this.address = address;
+                this.phone = phone;
+            }
+        }
+        #endregion
+        #region injection
+
+        public int Age
+        { 
+            get{ return this.age; }
+            set
+            {
+                if (value >= 18)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new Exception("Must be 18 or older to sign up!");
+                }
+            }
+        }
+        public int Phone
+        { 
+            get{ return this.phone; }
+            set
+            {
+                if (value != 0)
+                {
+                    this.phone = value;
+                }
+            }
+        }
+        public string Name
+        { 
+            get{ return this.name; }
+            set{ this.name = value; }
+        }
+        public string Address
+        { 
+            get{ return this.address; }
+            set{ this.address = value; }
+        }
+        #endregion
+        public void DataDisplay()
+        {
+            Console.WriteLine($"\nName: {this.name}\nAge: {this.age}\nPhone: {this.phone}\nAddress: {this.address}");
+        }
+
+    }
+}
---
+++ Classes/ClassObject/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ClassObject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int age, phone, verf=0;
+            string? name, add, input;
+            Pessoa pessoa=null;
+            Console.Write("Name: ");
+            name=Console.ReadLine();
+            
+            while (true)
+            {
+                Console.Write("Phone: ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out phone) && input.Length<=10)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Make sure the phone number has less then 10 digits.");
+                }
+            }
+            Console.Write("Address: ");
+            add = Console.ReadLine();
+            while (true)
+            {
+                Console.Write("Age: ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out age) && input.Length<=3)
+                {
+                    try
+                    {
+                        pessoa = new Pessoa(age, name, add, phone);
+                        break;
+                    }
+                    catch (Exception erro)
+                    {
+                        Console.WriteLine(erro.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid age.");
+                }
+            }
+            if (pessoa != null)
+            {
+                pessoa.DataDisplay();
+            }
+        }
+    }
+}
