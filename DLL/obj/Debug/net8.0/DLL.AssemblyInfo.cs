f4c6d3fecef73b13bf52b83328b69760f50418ca
NunoSilva <104792409+NunoRNx@users.noreply.github.com>
2024-10-11 15:45:42 +0100

    Folder Organization

--- DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
+++ DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b729d9e35eb58e08c2c6634a92c58e639e0012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4222df6e6cf297eb3f6c9ffdd0bbfa1557e094f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
